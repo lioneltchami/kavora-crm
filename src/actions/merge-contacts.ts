055610e0b86150ffd5a5a912a4bf2d334a654e4f
Kinglion <lioneltchami@gmail.com>
2026-09-24 18:52:12 -0600

    refactor(merge): [reviewer R2] drop unused redirect import

--- src/actions/merge-contacts.ts
+++ src/actions/merge-contacts.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import { inArray, sql } from "drizzle-orm";
 import { z } from "zod";
 import { db } from "@/db";
